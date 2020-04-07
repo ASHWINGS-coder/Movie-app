135c0cdd45de198ea7fc9e3a7484c60737780bc3
Ash <ashwings1996@gmail.com>
2020-04-07 10:30:15 +0530

    added fav

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,46 @@
-import {ADD_MOVIES} from '../actions'
+import {ADD_MOVIES ,ADD_TO_FAVOURITE , REMOVE_FROM_FAVOURITES ,SET_SHOW_FAVOURITES } from '../actions'
 
 const initialMoviesState = {
     list:[],
-    favourits:[]
+    favourites:[],
+    showFavorites:false
 }
 export default function movies (state = initialMoviesState, action) {
-    if(action.type === ADD_MOVIES){
-        return {
-            ...state,
-            list:action.movies
+    // if(action.type === ADD_MOVIES){
+    //     return {
+    //         ...state,
+    //         list:action.movies
+    //     }
+    // }
+    // return state;
+
+    switch(action.type){
+        case ADD_MOVIES:
+         return   {
+                ...state,
+                list: action.movies
         }
+        case ADD_TO_FAVOURITE:
+            return   {
+                   ...state,
+                   favourites: [action.movie , ...state.favourites]
+           }
+           case REMOVE_FROM_FAVOURITES:
+               const filterdArray = state.favourites.filter(
+                       movie => movie.Title !== action.movie.Title
+                   );
+                   return{
+                       ...state,
+                       favourites:filterdArray
+                   };
+            case SET_SHOW_FAVOURITES:
+                return{
+                     ...state,
+                    showFavorites: action.val
+                       }
+               
+        default:
+            return state;
     }
-    return state;
 }
 
